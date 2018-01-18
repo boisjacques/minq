8d266962f30cf0c57ce9822d18a34eb49c8ccaf4
Unknown <mail@manuelkieweg.de>
2018-01-18 14:49:34 +0100

    Added case flipper for test purposes

---
+++ bin/flipper/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"io/ioutil"
+)
+
+func main() {
+	filename := os.Args[0]
+	b, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Print(err)
+	}
+	for i, _ := range b {
+		if b[i] > 0x40 {
+			b[i] ^= 0x20
+		}
+	}
+	err = ioutil.WriteFile("flipped-" + filename, b, 0644)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
